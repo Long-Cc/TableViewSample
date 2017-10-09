728a4604342fb0917d404eae58e84d2aff6d12b5
I <mac@IdeMacBook-Pro.local>
2017-10-09 21:54:15 +0800

    Initial Commit

---
+++ TableViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewSample
+//
+//  Created by LongCh on 2017/10/9.
+//  Copyright © 2017年 LongCh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TableViewSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewSample
+//
+//  Created by LongCh on 2017/10/9.
+//  Copyright © 2017年 LongCh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
